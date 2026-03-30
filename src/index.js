354b36106af1200b8acadd50f712ef728b45e8c5
pepo20xd-sys <pepo20xd@gmail.com>
2026-03-29 20:00:18 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mi App</title>
+</head>
+<body>
+  <h1>Hola</h1>
+  <script src="app.js"></script>
+</body>
+</html>
